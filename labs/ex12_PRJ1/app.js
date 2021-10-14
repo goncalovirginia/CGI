6f0d15c9b4d55f32e1d2600c278bcac709cadf6a
xXParagoNXx <gonvirginia.blueslime@gmail.com>
2021-10-14 20:32:59 +0100

    ex12 clipping coordinates almost working (black canvas)

--- labs/ex12_PRJ1/app.js
+++ labs/ex12_PRJ1/app.js
@@ -1,37 +1,62 @@
 import * as UTILS from '../../libs/utils.js';
-import * as MV from '../../libs/MV.js'
+import * as MV from '../../libs/MV.js';
 
 /** @type {WebGLRenderingContext} */
+const canvas = document.getElementById("gl-canvas");
+let program;
 let gl;
-const table_width = 3.0;
-let table_height;
 
-function animate(time)
-{
-    gl.clear(gl.COLOR_BUFFER_BIT);
+const tableWidth = 3.0;
+let tableHeight;
+
+let points = [];
+const gridSpacing = 0.05;
+
+function resizeCanvas() {
+	canvas.width = window.innerWidth;
+  	canvas.height = window.innerHeight;
+	tableHeight = (tableWidth * canvas.height) / canvas.width;
 }
 
-function setup(shaders)
-{
-    const canvas = document.getElementById("gl-canvas");
+function setup(shaders) {
+	resizeCanvas();
+	window.addEventListener("resize", resizeCanvas);
+
     gl = UTILS.setupWebGL(canvas);
 
-	table_height = window.innerHeight;
+    program = UTILS.buildProgramFromSources(gl, shaders["shader1.vert"], shaders["shader1.frag"]);
 
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	for (let x = - tableWidth / 2; x <= tableWidth/2; x += gridSpacing) {
+    	for (let y = - tableHeight / 2; y <= tableHeight/2; y += gridSpacing) {
+        	points.push(MV.vec2(x, y));
+    	}
+	}
 
-	window.addEventListener("resize", function (event) {
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-	});
+	const buffer = gl.createBuffer();
+  	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  	gl.bufferData(gl.ARRAY_BUFFER, MV.flatten(points), gl.STATIC_DRAW);
 
-    const program = UTILS.buildProgramFromSources(gl, shaders["shader1.vert"], shaders["shader1.frag"]);
+  	const vPosition = gl.getAttribLocation(program, "vPosition");
+  	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+  	gl.enableVertexAttribArray(vPosition);
 
-    gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     
     window.requestAnimationFrame(animate);
+}
+
+function animate(time) {
+	window.requestAnimationFrame(animate);
+
+	gl.useProgram(program);
+
+	gl.uniform1f(gl.getUniformLocation(program, "tableWidth"), tableWidth);
+	gl.uniform1f(gl.getUniformLocation(program, "tableHeight"), tableHeight);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+  	gl.drawArrays(gl.POINTS, 0, points.length * 2);
 }
 
 UTILS.loadShadersFromURLS(["shader1.vert", "shader1.frag"]).then(s => setup(s));
--- labs/ex12_PRJ1/shaders/shader1.vert
+++ labs/ex12_PRJ1/shaders/shader1.vert
@@ -1,6 +1,10 @@
 attribute vec4 vPosition;
 
+uniform float tableWidth;
+uniform float tableHeight;
+
 void main()
 {
-    gl_Position = vPosition;
+    gl_Position = vPosition / vec4(tableWidth/2.0, tableHeight/2.0, 0.0, 0.0);
+    gl_PointSize = 4.0;
 }
